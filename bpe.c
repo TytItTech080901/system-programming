e764d271bb6a0c83af5927ddfd0954bd87bb82fe
TytItTech080901 <Yitaotian080901@outlook.com>
2026-08-02 16:10:53 +0800

    完善bpe逻辑

--- bpe.c
+++ bpe.c
@@ -16,18 +16,18 @@
 
 typedef struct {
 	int l, r;
-} PAIR_T;
+} Pair_t;
 
 typedef struct {
-	PAIR_T key;
+	Pair_t key;
 	size_t value;
 	UT_hash_handle hh;
-} KV_TABLE_T;
+} KV_table_t;
 
 int cmp_func(const void *a, const void *b)
 {
-	KV_TABLE_T *pa = *(KV_TABLE_T **)a;
-	KV_TABLE_T *pb = *(KV_TABLE_T **)b;
+	KV_table_t *pa = *(KV_table_t **)a;
+	KV_table_t *pb = *(KV_table_t **)b;
 	return pb->value - pa->value;
 }
 
@@ -36,85 +36,108 @@ int main()
 	const char *s =
 		"How do we construct reliable, portable, efficient, and secure computer systems? An essential component is the computer's operating system — the software that manages a computer's resources. First, the bad news: operating systems concepts are among the most complex in computer science. A modern, general-purpose operating system can exceed 50 million lines of code, or in other words, more than a thousand times longer than this textbook. New operating systems are being written all the time: if you use an e-book reader, tablet, or smartphone, an operating system is managing your device. Given this inherent complexity, we limit our focus to the essential concepts that every computer scientist should know.";
 
-	KV_TABLE_T *freq = NULL;
-	PAIR_T *pPairTable = NULL;
+	KV_table_t *freq = NULL;
+	Pair_t *pair_table = NULL;
 	for (int i = 0; i < 256; i++) {
-		boe_array_add(pPairTable, (PAIR_T){ .l = i });
+		boe_array_add(pair_table, (Pair_t){ .l = i });
 	}
 
-	unsigned char *pTokensIn = NULL;
-	unsigned int *pTokensOut = NULL;
+	unsigned int *tokens_in = NULL;
+	unsigned int *tokens_out = NULL;
 
-	size_t uTokensLen = strlen(s);
-	for (size_t i = 0; i < uTokensLen; i++) {
-		boe_array_add(pTokensIn, (unsigned char)s[i]);
+	size_t tokens_len = strlen(s);
+	for (size_t i = 0; i < tokens_len; i++) {
+		boe_array_add(tokens_in, (unsigned int)s[i]);
 	}
 
-	// LOG_DEBUG("%c", pTokensIn[uTokensLen - 1]);
+	// LOG_DEBUG("%c", tokens_in[tokens_len - 1]);
 
-	for (int i = 0; i < uTokensLen - 1; i += 1) {
-		PAIR_T k = { (unsigned char)pTokensIn[i], (unsigned char)pTokensIn[i + 1] };
-		KV_TABLE_T *entry = NULL;
+	int iter_cnt = 0;
+	for (;;) {
+		if (tokens_len <= 1)
+			break;
 
-		// struct key use HASH_FIND
-		HASH_FIND(hh, freq, &k, sizeof(PAIR_T), entry);
-		if (entry == NULL) {
-			entry = malloc(sizeof(KV_TABLE_T));
-			entry->key = k;
-			entry->value = 1;
-			HASH_ADD(hh, freq, key, sizeof(PAIR_T), entry);
-		} else {
-			entry->value++;
-		}
-	}
+		for (int i = 0; i < tokens_len - 1; i += 1) {
+			Pair_t k = { tokens_in[i], tokens_in[i + 1] };
+			KV_table_t *entry = NULL;
 
-	KV_TABLE_T maxPair = { 0 };
-	for (KV_TABLE_T *pCurrent = freq; pCurrent != NULL; pCurrent = pCurrent->hh.next) {
-		if (pCurrent->value > maxPair.value) {
-			maxPair.key = pCurrent->key;
-			maxPair.value = pCurrent->value;
-		}
-	}
-
-	LOG_INFO("(%u,%u) => %lu", maxPair.key.l, maxPair.key.r, maxPair.value);
-
-	boe_array_add(pPairTable, maxPair.key);
-
-	size_t uTokensIdx = 0;
-	while (uTokensIdx < uTokensLen - 1) {
-		PAIR_T tPair = { .l = pTokensIn[uTokensIdx], .r = pTokensIn[uTokensIdx + 1] };
-		if (memcmp(&tPair, &maxPair.key, sizeof(maxPair.key)) == 0) {
-			boe_array_add(pTokensOut, boe_array_len(pPairTable) - 1);
-			uTokensIdx += 2;
-		} else {
-			boe_array_add(pTokensOut, pTokensIn[uTokensIdx]);
-			if (uTokensIdx == uTokensLen - 2) {
-				boe_array_add(pTokensOut, pTokensIn[uTokensIdx + 1]);
+			// struct key use HASH_FIND
+			HASH_FIND(hh, freq, &k, sizeof(Pair_t), entry);
+			if (entry == NULL) {
+				entry = malloc(sizeof(KV_table_t));
+				entry->key = k;
+				entry->value = 1;
+				HASH_ADD(hh, freq, key, sizeof(Pair_t), entry);
+			} else {
+				entry->value++;
 			}
-			uTokensIdx++;
 		}
-	}
 
-	size_t uTokensOutLen = boe_array_len(pTokensOut);
-	LOG_INFO("pTokensOut: ");
-	for (size_t i = 0; i < uTokensOutLen; i++) {
-		if (pTokensOut[i] < 256)
-			printf("%c", pTokensOut[i]);
-		else
-			printf("<%u>", pTokensOut[i]);
+		KV_table_t max_pair = { 0 };
+		for (KV_table_t *cur = freq; cur != NULL; cur = cur->hh.next) {
+			if (cur->value > max_pair.value) {
+				max_pair.key = cur->key;
+				max_pair.value = cur->value;
+			}
+		}
+
+		// LOG_INFO("(%u,%u) => %lu", max_pair.key.l, max_pair.key.r, max_pair.value);
+
+		boe_array_add(pair_table, max_pair.key);
+
+		size_t tokens_idx = 0;
+		while (tokens_idx < tokens_len - 1) {
+			Pair_t tPair = { .l = tokens_in[tokens_idx], .r = tokens_in[tokens_idx + 1] };
+			if (memcmp(&tPair, &max_pair.key, sizeof(max_pair.key)) == 0) {
+				boe_array_add(tokens_out, boe_array_len(pair_table) - 1);
+				tokens_idx += 2;
+			} else {
+				boe_array_add(tokens_out, tokens_in[tokens_idx]);
+				if (tokens_idx == tokens_len - 2) {
+					boe_array_add(tokens_out, tokens_in[tokens_idx + 1]);
+				}
+				tokens_idx++;
+			}
+		}
+
+		// size_t tokens_out_len = boe_array_len(tokens_out);
+		// LOG_INFO("tokens_out: ");
+		// for (size_t i = 0; i < tokens_out_len; i++) {
+		// 	if (tokens_out[i] < 256)
+		// 		printf("%c", tokens_out[i]);
+		// 	else
+		// 		printf("<%u>", tokens_out[i]);
+		// }
+		// printf("\n");
+
+		KV_table_t *cur, *tmp;
+		HASH_ITER(hh, freq, cur, tmp)
+		{
+			HASH_DEL(freq, cur);
+			free(cur);
+		}
+
+		unsigned int *t = tokens_in;
+		tokens_in = tokens_out;
+		tokens_out = t;
+
+		boe_array_free(tokens_out);
+		tokens_out = NULL;
+		tokens_len = boe_array_len(tokens_in);
+		LOG_INFO("iter[%d] tokens_len: %zu", iter_cnt, tokens_len);
+		iter_cnt++;
 	}
-	printf("\n");
 
 	/* 排序 */
 	// size_t count = HASH_COUNT(freq);
-	// KV_TABLE_T **arr = malloc(count * sizeof(KV_TABLE_T *));
-	// KV_TABLE_T *cur;
+	// KV_table_t **arr = malloc(count * sizeof(KV_table_t *));
+	// KV_table_t *cur;
 	// size_t i = 0;
 	// for (cur = freq; cur != NULL; cur = cur->hh.next) {
 	// 	arr[i++] = cur;
 	// }
 
-	// qsort(arr, count, sizeof(KV_TABLE_T *), cmp_func);
+	// qsort(arr, count, sizeof(KV_table_t *), cmp_func);
 	// for (size_t i = 0; i < 10; i++) {
 	// 	printf("(%d,%d) => %zu\n", arr[i]->key.l, arr[i]->key.r, arr[i]->value);
 	// }
